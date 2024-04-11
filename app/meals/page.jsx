5cd3aaa834ad1625df66d0b55629c84bea5feb55
jitendra <jitendra199618@gmail.com>
2024-04-11 14:32:59 +0530

    xyz

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,5 +1,29 @@
-const Meals = () => {
-  return <div>Hello I&apos;m from Meals page</div>;
+import MealsGrid from "@/components/meals/meal-grids";
+import Link from "next/link";
+import classes from "@/app/meals/page.module.css";
+import { getMeals } from "@/libs/meals";
+
+const Meals = async () => {
+  const meals = await getMeals();
+  return (
+    <div>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipes and cook it yourself.It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your favorite recipes</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </div>
+  );
 };
 
 export default Meals;
--- components/main-header/nav-link.jsx
+++ components/main-header/nav-link.jsx
@@ -9,7 +9,11 @@ const NavLink = ({ href, children }) => {
   return (
     <Link
       href={href}
-      className={path.startsWith(href) ? classes.active : undefined}
+      className={
+        path.startsWith(href)
+          ? `font-normal font-sans bg-gradient-to-r from-violet-400 to-indigo-400 bg-clip-text text-transparent text-lg border-b-2 border-indigo-500`
+          : `font-normal font-sans text-slate-600 text-lg hover:bg-gradient-to-r hover:from-violet-400 hover:to-indigo-400 hover:bg-clip-text hover:text-transparent hover:border-b-2 hover:border-indigo-500`
+      }
     >
       {children}
     </Link>
